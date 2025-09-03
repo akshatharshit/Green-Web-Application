b24cff445d47dde15751dd50fb4432ffc81f820a
Akshat_Singh <akshatsingh132004@gmail.com>
2025-09-04 01:03:06 +0530

    Add a routes in frontend for uploading certificate and deleting them so that it can be use in future refrence

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -348,6 +348,6 @@ def predict_fertilizer():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 7000))  # use Render's PORT, fallback to 5000 locally
+    port = int(os.environ.get("PORT", 5000))  # use Render's PORT, fallback to 5000 locally
     app.run(host="0.0.0.0", port=port)
 
